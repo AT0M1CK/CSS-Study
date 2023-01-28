ed134d19d3a11cdc487ce4a53823b908a43b72e3
Akshayanand Vm <50578479+AT0M1CK@users.noreply.github.com>
2023-01-28 22:12:02 +0530

    refactor: added mobile nav

--- CSS-Background-Study/shared.js
+++ CSS-Background-Study/shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNo = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 // selecting all the buttons inside the plan class
 
@@ -12,7 +14,10 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
   });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
 
 modalNo.addEventListener("click", closeModal);
 
@@ -20,3 +25,8 @@ function closeModal() {
   backdrop.style.display = "none";
   modal.style.display = "none";
 }
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
