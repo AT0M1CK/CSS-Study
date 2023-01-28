9a42a4ca7c2c5bbae7fc219b5b9d7d3d699bb839
Akshayanand Vm <50578479+AT0M1CK@users.noreply.github.com>
2023-01-28 22:28:13 +0530

    refactor: cleaned up code

--- CSS-Background-Study/shared.js
+++ CSS-Background-Study/shared.js
@@ -23,12 +23,16 @@ backdrop.addEventListener("click", function () {
   closeModal();
 });
 
-modalNo.addEventListener("click", closeModal);
+if (modalNo) {
+  modalNo.addEventListener("click", closeModal);
+}
 
 function closeModal() {
   //   backdrop.style.display = "none";
   //   modal.style.display = "none";
-  modal.classList.remove("open");
+  if (modal) {
+    modal.classList.remove("open");
+  }
   backdrop.classList.remove("open");
 }
 
